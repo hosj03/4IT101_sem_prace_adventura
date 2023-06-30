1bf84f42bb7e1bf31ce3a1880d4d852c906d2850
hosj03 <73156188+hosj03@users.noreply.github.com>
2023-06-30 16:37:42 +0200

    uploaded 4IT101 aventura

---
+++ src/main/Start.java
@@ -0,0 +1,33 @@
+/* Soubor je ulozen v kodovani UTF-8.
+ * Kontrola kódování: Příliš žluťoučký kůň úpěl ďábelské ódy. */
+package main;
+
+
+
+import logika.*;
+import uiText.TextoveRozhrani;
+
+/*******************************************************************************
+ * Třída  Start je hlavní třídou projektu,
+ * který představuje jednoduchou textovou adventuru určenou k dalším úpravám a rozšiřování
+ *
+ * @author    Jarmila Pavlíčková, Jakub Hošek
+ * @version   20. 5. 2020
+ */
+public class Start
+{
+    /***************************************************************************
+     * Metoda, prostřednictvím níž se spouští celá aplikace.
+     *
+     * @param args Parametry příkazového řádku
+     */
+    public static void main(String[] args) {
+        IHra hra = new Hra();
+        TextoveRozhrani ui = new TextoveRozhrani(hra);
+        if (args.length == 0) {
+            ui.hraj();
+        } else {
+            ui.hrajZeSouboru(args[0]);
+        }
+    }
+}
